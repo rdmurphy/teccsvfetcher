c716f1285803b993175863280551ef7878656384
Ryan Murphy <github@accounts.rdmurphy.org>
2015-07-24 14:33:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TEC CSV Fetcher
+
+A library for interacting with the [Texas Ethics Commission](https://www.ethics.state.tx.us/)'s [new CSV format](https://www.ethics.state.tx.us/software/CampaignFinanceCSVFileFormat.pdf).
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='teccsvfetcher',
+    packages=find_packages(),
+    version='0.0.1',
+    license='MIT',
+    author='Ryan Murphy',
+    author_email='ryan@rdmurphy.org',
+    url='https://github.com/rdmurphy/teccsvfetcher',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python',
+    ]
+)
